ffaf668a5759f9bf8994cc9e08d7ef8eddfda963
Matt Richmond <matt@crowdfavorite.com>
2013-07-31 12:26:38 -0600

    changing name and description, so Page.ly won't overwrite the plugin automatically

--- interstitial-ads.php
+++ interstitial-ads.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Interstitial Ads
+Plugin Name: Interstitial Ads (Supports Categories)
 Plugin URI: http://wpmanage.com/interstitial-ads
-Description: Allows you to show Interstitial Ads on any WordPress site
+Description: Allows you to show Interstitial Ads on any WordPress site.  This has been modified by Crowd Favorite to support categories more reliably than the PRO version of the plugin.
 Version: 1.2-categories
 Author: Raul Ujog
 Author URI: http://wpmanage.com/
